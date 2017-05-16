fecf4bd995956dbd670ddc826c3294f259ec27b3
Jieyi (Chloe) Chen <chloechen1995@gmail.com>
2017-05-15 23:28:32 -0700

    update tweet.py with accounts that have no tweets

--- tweet.py
+++ tweet.py
@@ -15,7 +15,10 @@ final_data = pd.read_csv(open('final data.csv', 'rU'),
 # create a list of user ids
 id_list = list(final_data['id'])
 
+
 id_list.sort()
+
+id_list_split = [id_list[i:i + 500] for i in xrange(0, len(id_list), 500)]
 
 # Use reference: https://gist.github.com/yanofsky/5436496#file-tweet_dumper-py
 
@@ -23,21 +26,22 @@ def get_all_tweets(twitter_id):
     all_tweets = []
     tweets = api.user_timeline(user_id = twitter_id,count = 200)
     all_tweets.extend(tweets)
-    oldest = all_tweets[-1].id - 1
-    while len(tweets) > 0:
-        tweets = api.user_timeline(user_id = twitter_id,count = 200, max_id = oldest)
-        all_tweets.extend(tweets)
+    if len(all_tweets) > 0:
         oldest = all_tweets[-1].id - 1
-    outtweets = [[tweet.id_str, tweet.created_at, tweet.source, tweet.favorite_count, tweet.retweet_count, tweet.text.encode("utf-8")] for tweet in all_tweets]
-    tweets = [tweets + [twitter_id] for tweets in outtweets]
-    with open('%s_tweets.csv' % twitter_id, 'wb') as f:
-        writer = csv.writer(f, encoding='utf-8')
-        writer.writerow(["id","created_at", "source", "favorite_count", "retweet_count", "tweet_text", "id"])
-        writer.writerows(tweets)
-    
-    pass
+        while len(tweets) > 0:
+            tweets = api.user_timeline(user_id = twitter_id,count = 200, max_id = oldest)
+            all_tweets.extend(tweets)
+            oldest = all_tweets[-1].id - 1
+        outtweets = [[tweet.id_str, tweet.created_at, tweet.source, tweet.favorite_count, tweet.retweet_count, tweet.text.encode("utf-8")] for tweet in all_tweets]
+        tweets = [tweets + [twitter_id] for tweets in outtweets]
+        with open('%s_tweets.csv' % twitter_id, 'wb') as f:
+            writer = csv.writer(f, encoding='utf-8')
+            writer.writerow(["id","created_at", "source", "favorite_count", "retweet_count", "tweet_text", "id"])
+            writer.writerows(tweets)
 
-for i, user_id in enumerate(id_list):
+        pass
+
+for i, user_id in enumerate(id_list_split[1]):
     try:
         get_all_tweets(user_id)
         if ((i+1) % 100 == 0):
